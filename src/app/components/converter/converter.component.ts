c80aab5f11d685b2c1acd7eae45a9a76e715a687
PuntusYehor <maingithubacc@gmail.com>
2022-10-08 15:07:33 +0300

    Final version

--- src/app/components/converter/converter.component.ts
+++ src/app/components/converter/converter.component.ts
@@ -8,57 +8,56 @@ import { IRate } from '../../models/rates';
 })
 export class ConverterComponent implements OnInit{
   @Input() rates: IRate[]
-  loading = false
   reactiveForm: FormGroup;
   currentRate: string
-  c1: IRate | undefined
-  c2: IRate | undefined
-  arr: IRate[]
+  private c1: IRate | undefined
+  private c2: IRate | undefined
+  private arr: IRate[]
 
   ngOnInit(): void {
     this.reactiveForm = new FormGroup({
-      firstCurrNumber: new FormControl(null),
-      secondCurrNumber: new FormControl(null),
-      firstCurrString: new FormControl("UAH"),
-      secondCurrString: new FormControl("UAH")
+      firstCurrValue: new FormControl(null),
+      secondCurrValue: new FormControl(null),
+      firstCurrName: new FormControl("UAH"),
+      secondCurrName: new FormControl("UAH")
     })
   }
 
   fillCurr(): void {
     this.arr = this.rates
     this.arr = [...this.arr, {"rate":1, cc:"UAH"}]
-    this.c1 = this.arr.find(item => item.cc === this.reactiveForm.value.firstCurrString);
-    this.c2 = this.arr.find(item => item.cc === this.reactiveForm.value.secondCurrString);
+    this.c1 = this.arr.find(item => item.cc === this.reactiveForm.value.firstCurrName);
+    this.c2 = this.arr.find(item => item.cc === this.reactiveForm.value.secondCurrName);
+
+    if(this.c1 && this.c2) {
+      this.currentRate = `1 ${this.c1.cc} = ${(1 * this.c1.rate / this.c2.rate).toFixed(4)} ${this.c2.cc}`;
+    }
   }
 
   firstConvert(): void {
     if(this.c1 && this.c2) {
-      const res = +(this.reactiveForm.value.firstCurrNumber * this.c1.rate / this.c2.rate).toFixed(2);
-      this.reactiveForm.patchValue({secondCurrNumber: res})
-      this.currentRate = `1 ${this.c1.cc} = ${(1 * this.c1.rate / this.c2.rate).toFixed(4)} ${this.c2.cc}`;
+      const res = +(this.reactiveForm.value.firstCurrValue * this.c1.rate / this.c2.rate).toFixed(2);
+      this.reactiveForm.patchValue({secondCurrValue: res})
     }
   }
 
   secondConvert(): void {
     if(this.c1 && this.c2) {
-      const res = +(this.reactiveForm.value.secondCurrNumber * this.c2.rate / this.c1.rate).toFixed(2);
-      this.reactiveForm.patchValue({firstCurrNumber: res})
-      this.currentRate = `1 ${this.c1.cc} = ${(1 * this.c1.rate / this.c2.rate).toFixed(4)} ${this.c2.cc}`;
+      const res = +(this.reactiveForm.value.secondCurrValue * this.c2.rate / this.c1.rate).toFixed(2);
+      this.reactiveForm.patchValue({firstCurrValue: res})
     }
   }
 
   swap(): void {
-    let firstTmp = this.reactiveForm.value.firstCurrString
-    let secondTmp = this.reactiveForm.value.secondCurrString
-    this.reactiveForm.patchValue({firstCurrString: secondTmp})
-    this.reactiveForm.patchValue({secondCurrString: firstTmp})
+    let firstTmp = this.reactiveForm.value.firstCurrName
+    let secondTmp = this.reactiveForm.value.secondCurrName
+    this.reactiveForm.patchValue({firstCurrName: secondTmp, secondCurrName: firstTmp})
     this.fillCurr()
     this.firstConvert()
   }
 
   reset(): void {
-    this.reactiveForm.patchValue({firstCurrNumber: null})
-    this.reactiveForm.patchValue({secondCurrNumber: null})
+    this.reactiveForm.patchValue({firstCurrValue: null, secondCurrValue: null})
   }
 
 }
